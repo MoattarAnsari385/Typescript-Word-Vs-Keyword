a0d34172018889dd322c5d19a55cf81238c98a1d
Moattar Ansari <moattaransari62@gmail.com>
2024-11-19 07:35:01 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,40 @@
+// 1.variable 
+// code k andr ksi data ko store krny k liye variable use krty hen like : x,y,a,m,h.......
+
+let X1 = "Moattar Zubair"
+
+console.log(X1);
+console.log(X1);
+console.log(X1);
+console.log(X1);
+console.log(X1);
+
+
+
+
+// there are 3 types of variable
+// 1. let
+// 2.var
+// 3.const
+
+// Let : name change nh krskthy value kr skhty hen
+
+let myname:string = 'moattar'
+myname  = 'Fatima'
+console.log(myname)
+
+
+// var : name or value change kr skhty hen(ak hi name se bar bar bna skhty hen or bar bar val change bhi kr skhty hen)
+
+var fname:string = 'moattar'
+var fname:string = 'ansari'
+fname  = 'Fatima'
+myname = 'khan'
+console.log(myname)
+
+// const: name change  kr skhty hen value bhii.. constant rhy ga
+
+const lname:string = 'moattar'
+myname  = 'Fatima'
+console.log(myname)
+
